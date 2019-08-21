e5a569c11a2fa9dbd03b8b2cc0af916503b17d3b
AJAY <ajaymuktha@gmail.com>
2019-08-21 18:05:33 +0530

    app

---
+++ app/src/main/java/com/example/fountaneclub/RejectedApplication.java
@@ -0,0 +1,13 @@
+package com.example.fountaneclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RejectedApplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rejected_application);
+    }
+}
